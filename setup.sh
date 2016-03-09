e7b18167ed773bd36b70d1f7f97247f95d9cc3db
Tomás Pérez <t@0x101.com>
2016-03-09 11:27:49 +0100

    Adding bash-completion to the list of required packages.

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ sudo add-apt-repository ppa:unit193/encryption
 
 # Update and install packages
 sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install -y acpi secure-delete xsel curl git silversearcher-ag veracrypt
+sudo apt-get install -y acpi secure-delete xsel curl git silversearcher-ag veracrypt bash-completion
