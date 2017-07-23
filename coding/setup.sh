7c2d5599cc5581b11f88d8f775a48cd75260acb4
Tomás Pérez <t@0x101.com>
2017-07-23 11:18:56 +0200

    Adding ruby to the list of dependencies.

--- coding/setup.sh
+++ coding/setup.sh
@@ -3,4 +3,4 @@ curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 
 # Update and install packages
 sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install -y cmake python-dev exuberant-ctags nodejs
+sudo apt-get install -y cmake python-dev exuberant-ctags nodejs ruby-full
